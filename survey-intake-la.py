09e55ff8465fb6ca2971b4b76fad00b479f71ead
Randy Lubin <randylubin@gmail.com>
2017-12-05 13:34:56 -0800

    added NYC fields to Survey

--- survey-intake-la.py
+++ survey-intake-la.py
@@ -24,9 +24,28 @@ with open('ceSurvey.csv', 'rU') as f:
         neighborhood = row['Neighborhood']
         bookingType = row['CE type']
 
-        bayAreaGuests = row['Bay Area guest emails'].split(',')
+        southernCaliforniaGuests = row['Southern California Emails'].split(',')
 
-        for localEmail in bayAreaGuests:
+        for localEmail in southernCaliforniaGuests:
+            person = []
+            person.append('Unknown')
+            person.append('Unknown')
+            person.append(localEmail)
+            person.append(neighborhood)
+            person.append('Southern CA')
+            person.append('Previous Customer')
+            if bookingType == 'Private':
+                person.append('Corporate')
+            elif bookingType == 'Public':
+                person.append('General')
+            else:
+                person.append('')
+
+            people.append(person)
+
+        bayAreaGuests = row['Northern California Emails'].split(',')
+
+        for bayAreaEmail in bayAreaGuests:
             person = []
             person.append('Unknown')
             person.append('Unknown')
@@ -43,7 +62,26 @@ with open('ceSurvey.csv', 'rU') as f:
 
             people.append(person)
 
-        visitingGuests = row['Visiting guest emails'].split(',')
+        newYorkAreaGuests = row['NYC tri-state area emails'].split(',')
+
+        for NYCEmails in newYorkAreaGuests:
+            person = []
+            person.append('Unknown')
+            person.append('Unknown')
+            person.append(NYCEmails)
+            person.append(neighborhood)
+            person.append('NY, NJ, CONN')
+            person.append('Previous Customer')
+            if bookingType == 'Private':
+                person.append('Corporate')
+            elif bookingType == 'Public':
+                person.append('General')
+            else:
+                person.append('')
+
+            people.append(person)
+
+        visitingGuests = row['Other emails:'].split(',')
 
         for visitorEmail in visitingGuests:
             person = []
